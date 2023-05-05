f818c68100e694d6a6d8324b02ddbc55adae053b
Iyad Ahmed <iyadahmed430@gmail.com>
2023-05-05 21:40:14 +0300

    Format cytron_parser.c

--- cytron_parser.c
+++ cytron_parser.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-#include <stddef.h>
-#include <stdbool.h>
 #include <locale.h>
-#include <uchar.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
 #include <string.h>
+#include <uchar.h>
 
 #ifdef _MSC_VER
 #include <windows.h>
@@ -14,28 +14,25 @@
 
 STRUCT(buffer_t)
 {
-    char *mem;
+    char* mem;
     size_t cap, size;
 };
 
 buffer_t create_buffer()
 {
-    char *mem = malloc(1);
-    if (mem == NULL)
-    {
+    char* mem = malloc(1);
+    if (mem == NULL) {
         bail("Failed to allocate memory");
     }
-    return (buffer_t){mem, 1, 0};
+    return (buffer_t) { mem, 1, 0 };
 }
 
-void push_back_to_buffer(buffer_t *buffer, char value)
+void push_back_to_buffer(buffer_t* buffer, char value)
 {
-    if (buffer->size == buffer->cap)
-    {
+    if (buffer->size == buffer->cap) {
         buffer->cap *= 2;
         buffer->mem = realloc(buffer->mem, buffer->cap);
-        if (buffer->mem == NULL)
-        {
+        if (buffer->mem == NULL) {
             bail("Failed to re-allocate memory");
         }
     }
@@ -43,72 +40,64 @@ void push_back_to_buffer(buffer_t *buffer, char value)
     buffer->mem[buffer->size] = value;
 }
 
-void free_buffer(buffer_t *buffer)
+void free_buffer(buffer_t* buffer)
 {
     free(buffer->mem);
 }
 
-char *allocate_from_buffer(buffer_t *buffer, size_t n)
+char* allocate_from_buffer(buffer_t* buffer, size_t n)
 {
-    if ((buffer->size + n) > buffer->cap)
-    {
+    if ((buffer->size + n) > buffer->cap) {
         buffer->cap = (buffer->cap + n) * 2;
         buffer->mem = realloc(buffer->mem, buffer->cap);
-        if (buffer->mem == NULL)
-        {
+        if (buffer->mem == NULL) {
             bail("Failed to re-allocate memory");
         }
     }
 
-    char *result = (buffer->mem + buffer->size);
+    char* result = (buffer->mem + buffer->size);
     buffer->size += n;
     return result;
 }
 
-void truncate_and_shrink_buffer(buffer_t *buffer, size_t new_size)
+void truncate_and_shrink_buffer(buffer_t* buffer, size_t new_size)
 {
-    if (new_size > buffer->size)
-    {
+    if (new_size > buffer->size) {
         bail("New size for truncation has to be less than buffer size");
     }
 
     buffer->size = new_size;
     buffer->cap = new_size;
     buffer->mem = realloc(buffer->mem, new_size);
-    if (buffer->mem == NULL)
-    {
+    if (buffer->mem == NULL) {
         bail("Failed to re-allocate memory");
     }
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
 
 #ifdef _MSC_VER
     SetConsoleOutputCP(CP_UTF8);
 #endif
 
-    if (argc != 2)
-    {
+    if (argc != 2) {
         puts("Expected arguments: source.cy");
         return 0;
     }
 
-    FILE *file = fopen(argv[1], "rb");
-    if (file == NULL)
-    {
+    FILE* file = fopen(argv[1], "rb");
+    if (file == NULL) {
         bail("Failed to open file");
     }
 
     // Read entire source code into memory
     buffer_t buffer = create_buffer();
     size_t total_read_bytes = 0;
-    while (true)
-    {
-        char *dst = allocate_from_buffer(&buffer, 1024);
+    while (true) {
+        char* dst = allocate_from_buffer(&buffer, 1024);
         size_t num_read_bytes = fread(dst, 1, 1024, file);
-        if (num_read_bytes == 0)
-        {
+        if (num_read_bytes == 0) {
             break;
         }
         total_read_bytes += num_read_bytes;
@@ -119,14 +108,12 @@ int main(int argc, char **argv)
     printf("Parsing source:\n%s\n", buffer.mem);
 
     // Iterate source code one UTF-8 code point at a time
-    char *current_token_start = buffer.mem;
-    char *current_token_end = current_token_start;
-    while (true)
-    {
+    char* current_token_start = buffer.mem;
+    char* current_token_end = current_token_start;
+    while (true) {
         decode_code_point(&current_token_end);
 
-        if (memcmp(current_token_start, "import", current_token_end - current_token_start) == 0)
-        {
+        if (memcmp(current_token_start, "import", current_token_end - current_token_start) == 0) {
             puts("Found import token");
             current_token_start = current_token_end;
         }
@@ -137,8 +124,7 @@ int main(int argc, char **argv)
         // }
         // putchar('\n');
 
-        if (current_token_end == buffer.mem + total_read_bytes)
-        {
+        if (current_token_end == buffer.mem + total_read_bytes) {
             break;
         }
     }
